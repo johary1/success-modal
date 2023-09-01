5d98bfee032fd7dd544a899131a75c8a8d13d234
johary1 <joharisoa@gmail.com>
2023-09-01 15:00:06 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import { React, useState } from "react";
+import { ModalComponent } from "./ModalComponent";
 
-const App = () => {
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
   return (
-    <div>
-      <h1>My modal</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Customizable Modal Component Example</h1>
+        <button onClick={handleShowModal}>Show Modal</button>
+        <ModalComponent show={showModal} handleClose={handleCloseModal} />
+      </header>
     </div>
   );
-};
+}
 
 export default App;
